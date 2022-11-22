fa119bf77528e5dd27e70037d934c0203b4bd2bc
Lee <lee.jensen@agilix.com>
2022-11-21 19:31:45 -0700

    add(scores) JSX to populate scores from server or localstorage

--- src/scores.jsx
+++ src/scores.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
 
 export class Scores extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scores: [],
+    };
+
+    // Get the latest high scores from the service
+    fetch('/api/scores')
+      .then((response) => response.json())
+      .then((scores) => {
+        this.setState({ scores: JSON.parse(scores) });
+        // Save the scores in case we go offline in the future
+        localStorage.setItem('scores', JSON.stringify(scores));
+      })
+      .catch(() => {
+        // If there was an error then just use the last saved scores
+        const scoresText = localStorage.getItem('scores');
+        if (scoresText) {
+          this.setState({ scores: JSON.parse(scoresText) });
+        }
+      });
+  }
+
   render() {
+    const scores = [];
+    if (this.state.scores.length) {
+      for (const [i, score] of this.state.scores.entries()) {
+        scores.push(
+          <tr key={i}>
+            <td>{i}</td>
+            <td>{score.name}</td>
+            <td>{score.score}</td>
+            <td>{score.date}</td>
+          </tr>
+        );
+      }
+    } else {
+      scores.push(
+        <tr key='0'>
+          <td colSpan='4'>Be the first to score</td>
+        </tr>
+      );
+    }
+
     return (
       <main className='container-fluid bg-secondary text-center'>
         <table className='table table-warning table-striped-columns'>
@@ -13,7 +56,7 @@ export class Scores extends React.Component {
               <th>Date</th>
             </tr>
           </thead>
-          <tbody id='scores'></tbody>
+          <tbody id='scores'>{scores}</tbody>
         </table>
       </main>
     );
