80292ccc22ca3203c7557e5d89baa7d98a03f315
Lee <lee.jensen@agilix.com>
2022-11-21 20:15:40 -0700

    add(about) conversion to JSX

--- src/about.jsx
+++ src/about.jsx
@@ -1,12 +1,50 @@
 import React from 'react';
-import "./about.css"
+import './about.css';
 
 export class About extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageUrl: '',
+      quote: 'loading...',
+      quoteAuthor: 'unknown',
+    };
+
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#picture');
+
+        const width = containerEl.offsetWidth;
+        const height = containerEl.offsetHeight;
+        const imageUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
+        this.setState({ imageUrl: imageUrl });
+      })
+      .catch();
+
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ quote: data.content, quoteAuthor: data.author });
+      })
+      .catch();
+  }
+
   render() {
+    let imgEl = '';
+
+    if (this.state.imageUrl) {
+      imgEl = <img src={this.state.imageUrl} alt='pretty' />;
+    }
+
     return (
       <main className='container-fluid bg-secondary text-center'>
         <div>
-          <div id='picture' className='picture-box'></div>
+          <div id='picture' className='picture-box'>
+            {imgEl}
+          </div>
 
           <p>
             Simon is a repetative memory game where you follow the demonstrated
@@ -21,7 +59,10 @@ export class About extends React.Component {
             definition.
           </p>
 
-          <div id='quote' className='quote-box bg-light text-dark'></div>
+          <div className='quote-box bg-light text-dark'>
+            <p className='quote'>{this.state.quote}</p>
+            <p className='author'>{this.state.quoteAuthor}</p>
+          </div>
         </div>
       </main>
     );
