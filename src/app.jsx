52724d6fceff5e430c1a17d7b09c0c40e2fa7beb
Lee <lee.jensen@agilix.com>
2023-05-26 10:21:44 -0600

    cleanup() combined import statements

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Login } from './login/login';
 import { Play } from './play/play';
 import { Scores } from './scores/scores';
